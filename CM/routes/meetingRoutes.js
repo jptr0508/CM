791914e94217ad481bb995e3229fc6f1ee4c57d3
Tomás <tomasferreira91@gmail.com>
2021-11-06 17:49:44 +0000

    Metodos para criar uma concentração

--- CM/routes/meetingRoutes.js
+++ CM/routes/meetingRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mUtil = require("../models/meetingModel");
 
+
 router.get("/", async function (req, res, next) {
     let result = await mUtil.getAllConcents();
     res.status(result.status).send(result.result);
@@ -10,9 +11,14 @@ router.get("/", async function (req, res, next) {
 router.get("/:id", async function (req, res, next) {
     let id = req.params.id;
     let concentracoes = await mUtil.getConcentByUserID(id);
-    res.send(concentracoes);
+    res.status(result.status).send(result.result);
 });
-
+ router.post("/", async function (req, res, next){
+    let newMeet = req.body;
+    let result = await mUtil.saveUnit(newMeet);
+    console.log("saving unit with id: "+ newMeet.name);
+    res.status(result.status).send(result.result);;
+ })
 
 
 module.exports = router;
