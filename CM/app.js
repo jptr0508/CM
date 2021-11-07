ca7896236052d902d3b855103d54dbee4186cc13
Tomás <tomasferreira91@gmail.com>
2021-11-07 18:03:32 +0000

    Pagina de discussoes Doable

--- CM/app.js
+++ CM/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var utilizadoresRouter = require('./routes/userRoutes');
 var concentRouter = require('./routes/meetingRoutes');
+var discussionRouter = require('./routes/discussionRoutes');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/utilizadores',utilizadoresRouter);
 app.use('/api/concentracoes',concentRouter);
+app.use('/api/discussoes', discussionRouter);
 
 module.exports = app;
